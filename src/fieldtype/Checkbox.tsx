f2512efe6e00cee70b6fb872bf5883d6ff3f967e
Chen Xiongjie <chen.xcmail@gmail.com>
2020-06-27 10:38:09 +0200

    Added: show checkbox(inline)

--- src/fieldtype/Checkbox.tsx
+++ src/fieldtype/Checkbox.tsx
@@ -13,26 +13,44 @@ constructor(props:any) {
       this.setState({checked: !this.state.checked});
     }
 
+    edit(){
+      const BeforeElement:React.ReactType = this.props.beforeField();
+      const AfterElement:React.ReactType = this.props.afterField();
+      return (
+          <div className={'field checkbox '+this.props.mode+' '+(this.props.definition.required?'required':'')+(this.props.validation=='1'?' result-required':'')}>
+            {BeforeElement}
+           <label>
+              {this.props.mode=='edit'&&!this.state.checked&&<input type="hidden" value="0" name={this.props.definition.identifier} />}
+              <input type="checkbox" disabled={this.props.mode!='edit'}
+                  id={this.props.definition.identifier}
+                  name={this.props.definition.identifier}
+                  value="1"
+                  onChange={this.changeChecked.bind(this)}
+                  defaultChecked={this.props.data==1} />
+              <div>{this.props.definition.name}
+              {this.props.definition.description&&<i className="icon-info" data-for={this.props.definition.identifier+'-desciption'} data-tip=""></i>}</div>
+              {this.props.definition.description&&<ReactTooltip id={this.props.definition.identifier+'-desciption'} effect="solid" place="right" html={true} clickable={true} multiline={true} delayHide={500} className="tip">{this.props.definition.description}</ReactTooltip>}
+             </label>
+              {AfterElement}
+          </div>
+      )
+    }
+
+    raw(){
+      return (
+          <span className={'field checkbox'}>
+            <input type="checkbox" disabled={true}
+                   value="1"
+                   defaultChecked={this.props.data==1} />
+          </span>
+      )
+    }
+
     render(){
-        const BeforeElement:React.ReactType = this.props.beforeField();
-        const AfterElement:React.ReactType = this.props.afterField();
-        return (
-            <div className={'field checkbox '+this.props.mode+' '+(this.props.definition.required?'required':'')+(this.props.validation=='1'?' result-required':'')}>
-              {BeforeElement}
-             <label>
-                {this.props.mode=='edit'&&!this.state.checked&&<input type="hidden" value="0" name={this.props.definition.identifier} />}
-                <input type="checkbox" disabled={this.props.mode!='edit'}
-                    id={this.props.definition.identifier}
-                    name={this.props.definition.identifier}
-                    value="1"
-                    onChange={this.changeChecked.bind(this)}
-                    defaultChecked={this.props.data==1} />
-                <div>{this.props.definition.name}
-                {this.props.definition.description&&<i className="icon-info" data-for={this.props.definition.identifier+'-desciption'} data-tip=""></i>}</div>
-                {this.props.definition.description&&<ReactTooltip id={this.props.definition.identifier+'-desciption'} effect="solid" place="right" html={true} clickable={true} multiline={true} delayHide={500} className="tip">{this.props.definition.description}</ReactTooltip>}
-               </label>
-                {AfterElement}
-            </div>
-        )
+        if( this.props.mode == 'edit' || this.props.mode == 'view' ){
+          return this.edit();
+        }else{
+          return this.raw();
+        }
     }
 }
