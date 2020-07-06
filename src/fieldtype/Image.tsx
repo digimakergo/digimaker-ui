3d34c97521d580ef458c11d52fb84a35d8c87f16
Chen Xiongjie <chen.xcmail@gmail.com>
2020-07-06 12:39:25 +0200

    Added: support thumbnail in image

--- src/fieldtype/Image.tsx
+++ src/fieldtype/Image.tsx
@@ -15,7 +15,13 @@ constructor(props:any) {
     }
 
     inline(){
-      return this.props.data?<img src={process.env.REACT_APP_ASSET_URL+"/"+this.state.data} />:'';
+      if(this.state.data){
+        let thumbnailPath = process.env.REACT_APP_THUMB_PATH.replace("{imagepath}", this.state.data);
+        thumbnailPath = process.env.REACT_APP_ASSET_URL + "/"+thumbnailPath;
+        return <img src={thumbnailPath} />;
+      }else{
+        return '';
+      }
     }
 
     view(){
@@ -29,18 +35,19 @@ constructor(props:any) {
     edit(){
         return (
             <div className={(this.props.definition.required?'required':'')+(this.props.validation=='1'?' result-required':'')}>
-                <label htmlFor={this.props.definition.identifier}>{this.props.definition.name}
+                <label className="field-label" htmlFor={this.props.definition.identifier}>{this.props.definition.name}
                     {this.props.definition.description&&<i className="icon-info" data-for={this.props.definition.identifier+'-desciption'} data-tip=""></i>}
                     {this.props.definition.description&&<ReactTooltip id={this.props.definition.identifier+'-desciption'} effect="solid" place="right" html={true} clickable={true} multiline={true} delayHide={500} className="tip">{this.props.definition.description}</ReactTooltip>}
                 :</label>
 
-                <div className="edit-input">
+                <div className="field-value">
                 <FileUpload name={this.props.definition.identifier}
                                               service="content"
                                               format="image/*"
                                               value={this.props.data}
                                               onSuccess={(data)=>{this.updated(data)}} />
-                {this.inline()}
+                {(this.state.data==this.props.data) && this.inline()}
+                {(this.state.data!=this.props.data) && <img src={process.env.REACT_APP_ASSET_URL + "/"+this.state.data} /> }
                 </div>
             </div>
         )
