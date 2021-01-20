333b01f5b41bdb8e421bef3265a9c4a2ca317d92
Chen Xiongjie <chen.xcmail@gmail.com>
2021-01-20 17:43:52 +0100

    Added: selected is different with active in TreeNode & support selectedId as array

--- src/TreeNode.tsx
+++ src/TreeNode.tsx
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom"
 import Collapse from 'react-bootstrap/Collapse'
 
 //TreeNode which render tree based on data from server.
+//selected: number|array<number> for selected id
 //renderItem is to render what's inside(eg. if you want to remove icon or output node id, or additional link ).
-function TreeNode(props:{data:any, selectedId?:number, showRoot?:boolean, renderItem?:any, onClick?:any}) {
+function TreeNode(props:{data:any, selectedId?:any, showRoot?:boolean, renderItem?:any, onClick?:any}) {
   //todo: use better way to get parameters
   return <ul className="treemenu">
     {props.showRoot&&<TreeNodeItem data={props.data} selectedId={props.selectedId} renderItem={props.renderItem} onClick={props.onClick}/>}
@@ -14,10 +15,10 @@ function TreeNode(props:{data:any, selectedId?:number, showRoot?:boolean, render
   </ul>
 }
 
-class TreeNodeItem extends React.Component<{ data: any, selectedId?: number, onClick?:any, renderItem?:any, onOpenClose?:any }, { open: boolean}> {
+class TreeNodeItem extends React.Component<{ data: any, selectedId?: any, onClick?:any, renderItem?:any, onOpenClose?:any }, { open: boolean, selected:boolean}> {
   constructor(props: any) {
     super(props);
-    this.state = { open:false};
+    this.state = { open:false, selected: false};
   }
 
   //todo: fix that when visit from url directly it's closed - use router match to update state?
@@ -27,10 +28,20 @@ class TreeNodeItem extends React.Component<{ data: any, selectedId?: number, onC
     this.setState({ open: !this.state.open });
   }
 
+
+
   componentDidMount(){
+    this.initSelected();
+  }
+
+  initSelected(){
     let node = this.props.data;
-    if( node.id == this.props.selectedId ){
-      this.setState({open: true});
+    let selectedId = this.props.selectedId;
+    if( ( Number.isInteger( selectedId ) && node.id == selectedId ) ||
+        ( Array.isArray( selectedId ) && selectedId.includes( node.id ) )  ){
+      this.setState({open: true, selected: true});
+    }else{
+      this.setState({selected: false});
     }
   }
 
@@ -41,6 +52,11 @@ class TreeNodeItem extends React.Component<{ data: any, selectedId?: number, onC
           this.props.onOpenClose(true);
       }
     }
+
+    if( Number.isInteger( prevProps.selectedId ) && prevProps.selectedId != this.props.selectedId ||
+        Array.isArray( prevProps.selectedId ) && prevProps.selectedId.join(',') != this.props.selectedId.join(',') ){
+          this.initSelected();
+        }
   }
 
   onClick(e){
@@ -58,12 +74,14 @@ class TreeNodeItem extends React.Component<{ data: any, selectedId?: number, onC
     let open = this.state.open;
 
     let subtype = (node.fields && node.fields['subtype']) ? ('icon-subtype-' + node.fields['subtype']) : '';
-    return <li className={open ? 'tree-open' : 'tree-close'}>
-      <NavLink to={url} activeClassName="selected" onClick={(e)=>{this.onClick(e)}}>
+    return <li className={(open ? 'tree-open' : 'tree-close') + ' ' + (node.children?'tree-haschild':'tree-nochild')}>
+      <NavLink to={url} className={this.state.selected?'selected':''}
+                        activeClassName="active"                        
+                        onClick={(e)=>{this.onClick(e)}}>
         <span className={node.children ? 'foldable space' : 'space'} onClick={(e) => this.openclose(e)}>
           {node.children&&<i className={"foldable fas fa-chevron-right" + (open ? ' open' : '')}></i>}
        </span>
-        {this.props.renderItem?(this.props.renderItem(node)):(<span className="tree-text" title={node.name}><i className={"nodeicon far icon-" + node.content_type + " " + subtype}></i>{node.name}</span>)}
+        {this.props.renderItem?(this.props.renderItem(node)):(<span className="tree-text" title={node.name}><i className={"nodeicon far icon-" + node.content_type + " " + subtype}></i><span>{node.name}</span></span>)}
       </NavLink>
 
       {/*todo: load it without sliding*/}
