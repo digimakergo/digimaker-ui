b1d0461c4978c3bb212ddf1768d1c4ecacfea1aa
Chen Xiongjie <chen.xcmail@gmail.com>
2021-02-16 12:10:33 +0100

    Fixed: datetime auto close, support dateonly

--- src/fieldtype/Datetime.tsx
+++ src/fieldtype/Datetime.tsx
@@ -8,7 +8,7 @@ import { timeStamp } from 'console';
 
 let defaultValue = 0;
 
-export default class Datetime extends React.Component<{definition: any, validation: any, data: any, mode: string },{datetime, disabled, error:string}> {
+export default class Datetime extends React.Component<{definition: any, validation: any, data: any, mode: string },{datetime, dateOnly:boolean, error:string}> {
     constructor(props:any) {
         super(props);
         let datetime = null;
@@ -19,7 +19,9 @@ export default class Datetime extends React.Component<{definition: any, validati
             datetime = moment(props.data)
           }
         }
-        this.state = {datetime:datetime, disabled: false,error: ''};
+
+        let dateOnly = (this.props.definition.parameters && this.props.definition.parameters['dateonly'])?true:false;
+        this.state = {datetime:datetime, dateOnly: dateOnly,error: ''};
       }
 
       updateValue(inputtype:string, value:any){
@@ -68,16 +70,22 @@ export default class Datetime extends React.Component<{definition: any, validati
               </>;
       }
 
+      clear(e){
+        e.preventDefault();
+        this.setState({datetime:null});
+      }
+
       edit(){
         return (
             <div>
             <label className="field-label">{this.props.definition.name}</label>
             <div className="field-value">
-                <DateTime className='fieldtype-datetime-date' defaultValue={this.state.datetime?this.state.datetime:''} timeFormat={false} dateFormat="DD.MM.YYYY" onChange={value => this.updateValue('date',value)}/>
-                {!this.state.disabled&&<span> &nbsp;
+                <DateTime className='fieldtype-datetime-date' closeOnSelect={true} value={this.state.datetime?this.state.datetime:''} timeFormat={false} dateFormat="DD.MM.YYYY" onChange={value => this.updateValue('date',value)}/>
+                {!this.state.dateOnly&&<span> &nbsp;
                       <input className="fieldtype-datetime-time form-control" defaultValue={this.state.datetime?this.state.datetime.format('HH'):''} type = "text" maxLength={2} onChange={e => this.updateValue('hour',e.target.value)}/>
                       :<input className="fieldtype-datetime-time form-control" defaultValue={this.state.datetime?this.state.datetime.format('mm'):''} type="text" maxLength={2} onChange={e => this.updateValue('minute',e.target.value)}/>
                   </span>}
+                &nbsp; <a href="#" onClick={(e)=>this.clear(e)}><i className="far fa-trash-alt"></i></a>
                 {this.state.error&&<span className='error'>{this.state.error}</span>}
                 {!this.state.error&&<input type="hidden" name={this.props.definition.identifier} value={this.state.datetime?this.state.datetime.format('YYYY-MM-DD HH:mm'):''} />}
                 </div>
