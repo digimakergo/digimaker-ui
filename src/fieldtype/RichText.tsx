3696bb3beb59bb304dba9989ec9ee8472681dcf5
Chen Xiongjie <chen.xcmail@gmail.com>
2021-11-03 10:59:31 +0100

    updated: use react=tinymce way

--- src/fieldtype/RichText.tsx
+++ src/fieldtype/RichText.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import ReactTooltip from 'react-tooltip';
-import tinymce from "tinymce";
+import { Editor } from '@tinymce/tinymce-react';
 import Fieldtype from '../fieldtype.json';
 
-var mode = Fieldtype.richtext.mode.standard
+var toolbars = Fieldtype.richtext.mode.standard
 
-export default class RichText extends React.Component<{ definition: any, validation: any, data: any, mode: string }, {data:''}> {
+export default class RichText extends React.Component<{ definition: any, validation: any, data: any, mode: string }, {data:any}> {
+
+
+  private editorRef;
 
   constructor(props: any) {
     super(props);
+    this.editorRef = React.createRef();
     this.state = {
       data:'',
     };
@@ -17,40 +21,14 @@ export default class RichText extends React.Component<{ definition: any, validat
   componentDidMount() {
     if(this.props.definition.parameters &&this.props.definition.parameters.mode)
     {
-      mode= Fieldtype.richtext.mode[this.props.definition.parameters.mode];
+      toolbars= Fieldtype.richtext.mode[this.props.definition.parameters.mode];
     }
     else
     {
-      mode=Fieldtype.richtext.mode.standard;
+      toolbars=Fieldtype.richtext.mode.standard;
     }
 
-    if(this.props.mode=='edit'){
-      tinymce.init({
-        menubar:false,
-        toolbar: mode,
-        height: 300,
-        selector: `textarea#`+this.props.definition.identifier,
-        plugins: Fieldtype.richtext.plugins,
-        branding: false,
-        setup: editor => {
-          editor.on("keyup change", () => {
-            const content = editor.getContent();
-            this.setState({data:content})
-          });
-
-          editor.on("init", () => {
-            editor.setContent(this.props.data);
-          });
-        },
-      });
-    }
     this.setState({data:this.props.data});
-  }
-
-  componentWillUnmount() {
-    if(this.props.mode=='edit'){
-      tinymce.remove(`textarea#`+this.props.definition.identifier);
-    }
   }
 
   edit() {
@@ -60,7 +38,22 @@ export default class RichText extends React.Component<{ definition: any, validat
           {this.props.definition.description && <i className="icon-info" data-for={this.props.definition.identifier+'-desciption'} data-tip=""></i>}
           {this.props.definition.description&&<ReactTooltip id={this.props.definition.identifier+'-desciption'} effect="solid" place="right" html={true} clickable={true} multiline={true} delayHide={500} className="tip">{this.props.definition.description}</ReactTooltip>}
           : </label>
-          <textarea id={this.props.definition.identifier} className="field-value form-control" name={this.props.definition.identifier} value={this.state.data}></textarea>
+          <Editor
+            initialValue={this.props.data}                   
+            onChange={(e, editor)=>this.setState({data:editor.getContent()})}
+            init={{
+              menubar: false,
+              height:'300px',
+              plugins: Fieldtype.richtext.plugins,
+              external_plugins: {
+                    'dmimage': process.env.PUBLIC_URL+'/tinymce/dmimage.min.js',
+              },
+              branding: false,           
+              toolbar: toolbars,
+              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+            }}
+          />
+          <textarea readOnly={true} style={{display:'none'}} id={this.props.definition.identifier} className="field-value form-control" name={this.props.definition.identifier} value={this.state.data}></textarea>
           </>
     )
   }
