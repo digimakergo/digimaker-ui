02dbd417d393c3cf7fa22eadf38e81ef5c503fc2
Chen Xiongjie <chen.xcmail@gmail.com>
2022-09-02 20:40:11 +0200

    updated: move _old(use git history to restore if needed)

--- src/_old/FieldtypeIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function FieldtypeIcon(props: { contenttype:string }) {
-  return (<i className={"icon icon-contenttype icon-"+props.contenttype}></i>);
-}
-
-
-export default FieldtypeIcon;
